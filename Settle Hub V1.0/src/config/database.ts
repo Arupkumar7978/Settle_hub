7d3603031977652f4103604cc75483bc92bcf4b5
Subham1901 <subhamprasad1901@gmail.com>
2024-10-28 23:45:02 +0530

    Revert "chore: typescript setup"
    This reverts commit ccee143d1e3fcaf494c3ecdd0ced4fd0b4ed58e9.

--- Settle Hub V1.0/src/config/database.ts
+++ Settle Hub V1.0/src/config/database.ts
@@ -1,5 +1,5 @@
-import { Sequelize, Dialect } from "sequelize";
-import dotenv from "dotenv";
+import { Sequelize, Dialect } from 'sequelize';
+import dotenv from 'dotenv';
 
 interface DatabaseSequelizer {
   connect(): Sequelize;
@@ -11,11 +11,12 @@ dotenv.config();
 export default class DatabaseConnector implements DatabaseSequelizer {
   private sequelizeInstance: Sequelize;
 
-  private DB_NAME = process.env.DB_NAME || "";
-  private DB_USER = process.env.DB_USER || "";
-  private DB_HOST = process.env.DB_HOST || "";
-  private DB_PASSWORD = process.env.DB_PASSWORD || "";
-  private DB_DIALECT: Dialect = (process.env.DB_DIALECT || "mysql") as Dialect;
+  private DB_NAME = process.env.DB_NAME || '';
+  private DB_USER = process.env.DB_USER || '';
+  private DB_HOST = process.env.DB_HOST || '';
+  private DB_PASSWORD = process.env.DB_PASSWORD || '';
+  private DB_DIALECT: Dialect =
+    (process.env.DB_DIALECT as Dialect) || 'mysql';
 
   constructor() {
     this.sequelizeInstance = new Sequelize(
@@ -24,7 +25,7 @@ export default class DatabaseConnector implements DatabaseSequelizer {
       this.DB_PASSWORD,
       {
         host: this.DB_HOST,
-        dialect: this.DB_DIALECT || "mysql",
+        dialect: this.DB_DIALECT
       }
     );
   }
@@ -37,9 +38,9 @@ export default class DatabaseConnector implements DatabaseSequelizer {
   private async initiateDatabaseConnection() {
     try {
       await this.sequelizeInstance.authenticate();
-      console.log("Database Connection has been established successfully.");
+      console.log('Database Connection has been established successfully.');
     } catch (error) {
-      console.error("Unable to connect to the database:", error);
+      console.error('Unable to connect to the database:', error);
     }
   }
 }
